87c1df7864d5dfd29df01f89ddee32887166a601
theo m <theo@mantz.io>
2021-04-19 13:17:18 -0400

    Remove cached keys_dev

--- config/keys_dev.js
+++
@@ -1,7 +0,0 @@
-
-module.exports = {
-  mongoURI:
-    "mongodb+srv://dev:rFGme4BoReuIoAyM@cluster0.lgcvs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-  secretOrKey: "QkhKRz62QQF8pXoUPBTxxLVryZ59AFsh",
-};
-
