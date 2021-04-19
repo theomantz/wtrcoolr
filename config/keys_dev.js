6f24e68794b368ffd3561908575dd181ee58087f
theo m <theo@mantz.io>
2021-04-19 13:05:37 -0400

    Add config

---
+++ config/keys_dev.js
@@ -0,0 +1,7 @@
+
+module.exports = {
+  mongoURI:
+    "mongodb+srv://dev:rFGme4BoReuIoAyM@cluster0.lgcvs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  secretOrKey: "QkhKRz62QQF8pXoUPBTxxLVryZ59AFsh",
+};
+
