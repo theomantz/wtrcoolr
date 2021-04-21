f4ccbc1346b7ffca64c60f64d66e1febbec718c9
theo m <theo@mantz.io>
2021-04-20 21:40:14 -0400

    Work on components

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -5,7 +5,7 @@ import Modal from './modal/modal'
 import { Switch } from 'react-router-dom';
 import { AuthRoute, ProtectedRoute } from '../util/route_utils';
 import NavBarContainer from './nav-bar/nav_bar_container';
-import CoolrVideoContainer from './coolr_video/coolr_vide_container'
+import CoolrVideoContainer from './coolr_video/coolr_video_container'
 import Dashboard from './dashboard/dashboard'
 // import './reset-css.css'
 
