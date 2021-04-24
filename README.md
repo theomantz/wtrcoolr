4b6f9dc5d2dc68b5abe7cdca5bc672f71e5555f6
theo m <theo@mantz.io>
2021-04-24 10:12:48 -0400

    Add live link to readme

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 ## Introduction
 
 - wtrcoolr is a MERN Stack video chatting app which helps teams build bonds and get to know eachother while remaining remote
+
+
+# Live Link:
+
+[wtrcooler live](https://wtrcoolr.herokuapp.com/#/)
---
+++ frontend/src/components/about_us/about_us.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class AboutUs extends React.Compoent { 
+  render() {
+    return (
+      <div className='about-us-container'>
+        <header className='about-us-header'>
+          
+        </header>
+      </div>
+    )
+  }
+}
+
+export default AboutUs
