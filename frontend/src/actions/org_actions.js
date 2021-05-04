96399eeb7eb5e7b812acef24fbacc4750e378eac
heavyevy <everettsmith95@gmail.com>
2021-05-04 11:31:29 -0700

    Add clearErrors action to session_form

--- frontend/src/actions/org_actions.js
+++ frontend/src/actions/org_actions.js
@@ -1,22 +1,14 @@
 import * as APIUtil from "../util/orgs_api_util";
+import { receiveErrors } from "./session_actions";
 
 
-export const RECEIVE_SESSION_ERRORS = "RECEIVE_SESSION_ERRORS";
 export const RECEIVE_ORGS = 'RECEIVE_ORGS';
-
-
-const receiveErrors = (errors) => ({
-  type: RECEIVE_SESSION_ERRORS,
-  errors,
-});
 
 const receiveOrgs = orgs => ({
     type: RECEIVE_ORGS,
     orgs,
   });
   
-
-
 export const createOrg = (org) => (dispatch) => {
   return APIUtil.createOrg(org)
     .catch((err) => {
