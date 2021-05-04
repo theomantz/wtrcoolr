50ccc5bc45993c28baf1fd762217477ad8dfad39
heavyevy <everettsmith95@gmail.com>
2021-05-04 14:46:01 -0700

    Add leave button to orgs

--- frontend/src/components/dashboard/organization_list.js
+++ frontend/src/components/dashboard/organization_list.js
@@ -2,42 +2,75 @@ import React from "react";
 import './organization_list.css'
 import { Link } from "react-router-dom";
 
-const getItems = props => {
-  let userOrgs = props.state.session.user.orgs
-  let itemArr = userOrgs.map(org => (
-    {id: String(org._id), content: org.name}
-  ))
+// const getItems = props => {
+//   let userOrgs = props.state.session.user.orgs
+//   let itemArr = userOrgs.map(org => (
+//     {id: String(org._id), content: org.name}
+//   ))
 
-  return itemArr;
-}
+//   return itemArr;
+// }
 
 class OrganizationList extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      items: getItems(props)
-    };
+    // this.state = {
+    //   // items: getItems(props)
+    // };
+
+    this.handleClick = this.handleClick.bind(this);
   }
+
+  handleClick(org, currentUser) {
+    return (
+      e => {
+
+        const findOrgIndex = (org) => {
+          for (let i = 0; i < this.props.currentUser.orgs.length; i++) {
+            if (this.props.currentUser.orgs[i]._id === org._id) {
+              return i
+            }
+          }
+          return -1;
+        }
+        e.preventDefault();
+        let orgIndex = findOrgIndex(org)
+        let orgsArr = currentUser.orgs
+        orgsArr.splice(orgIndex, 1)
+        this.props.updateOrgUsers({
+          userId: currentUser.id,
+          orgId: org._id,
+          admin: false,
+          add: false
+        });
+        this.props.updateUser({
+          orgs: orgsArr,
+          id: currentUser.id
+        });
+      }
+    )
+  }
+
 
   render() {
 
-      let userOrgs = this.props.state.session.user.orgs
-      let itemArr = userOrgs.map(org => (
-        {id: String(org._id), content: org.name}
-      ))
-
-
+    // let userOrgs = this.props.userOrgs
+    let itemArr = this.props.itemArr
 
     return (
 
 
-            <div className="organization-list-container">
-              {itemArr.map((item, index) => (
-                <div className="dashboard-org-list">
-                  <strong>{item.content}</strong><Link className="admin-button" to={`admin/${item.id}`}>Admin</Link>
-                </div>
-              ))}
+      <div className="organization-list-container">
+        {itemArr.map((item, index) => (
+          <div className="dashboard-org-list">
+            <strong>{item.content}</strong>
+            <div className="buttons-list">
+              <Link className="admin-button" to={`admin/${item.id}`}>Admin</Link>
+              <button onClick={this.handleClick(item, this.props.currentUser)} className="leave-org-button">Leave</button>
             </div>
+          </div>
+        ))}
+      </div>
     )}
 
 }
---
+++ frontend/src/components/dashboard/organization_list_container.js
@@ -0,0 +1,21 @@
+import { connect } from 'react-redux';
+import OrganizationList from './organization_list';
+import { updateOrgUsers } from '../../actions/org_actions';
+import { updateUser } from '../../actions/users_actions';
+
+
+
+const mSTP = state => ({
+  currentUser: state.session.user,
+  // userOrgs: state.session.user.orgs,
+  itemArr: state.session.user.orgs.map(org => (
+    { id: String(org._id), content: org.name }
+  ))
+})
+
+const mDTP = dispatch => ({
+  updateOrgUsers: org => dispatch(updateOrgUsers(org)),
+  updateUser: user => dispatch(updateUser(user))
+})
+
+export default connect(mSTP, mDTP)(OrganizationList);
