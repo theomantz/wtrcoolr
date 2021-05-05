1bcf5f47db85fca03441d72e0ca31e3f43184088
jhnegbrt <jhnegbrt@gmail.com>
2021-05-05 11:09:23 -0700

    adds styling to welcome modal

--- frontend/src/components/splash/welcome.js
+++ frontend/src/components/splash/welcome.js
@@ -6,7 +6,7 @@ const Welcome = () =>{
     <div className="welcome-modal">
       <h2>Welcome to wtrcoolr</h2>
       <div class="about">
-        <h3>About</h3>
+        <h3>About:</h3>
         <p>This application was inspired by the chance encounters we all miss so much as a result of working from home
           and quarantining.</p>
         <p>Whether it was connecting with a former team-member at work or chatting with your child's friend's parents
@@ -17,6 +17,10 @@ const Welcome = () =>{
       </div>
       <div class="instructions">
         <h3>Instructions:</h3>
+        <h4>Demo</h4>
+        <p>Explore wtrcoolr by entering as a demo user. Invite a friend or coworker to use the other demo account and you'll likely
+          end up paired together in a 'coolrhour'
+        </p>
         <h4>Sign Up/Log In</h4>
         <p>
           Enter with your own account! You can create organizations, designate specific times for 
@@ -25,10 +29,6 @@ const Welcome = () =>{
         <p>
           If you are logged in and belong to an organization with an active 'coolrhour' you'll be prompted to join a zoom style 
           video chat with another online member (chosen randomly) from the same organization.
-        </p>
-        <h4>Demo</h4>
-        <p>Alternatively, you can enter as a demo user. Invite a friend or coworker to use the other demo account and you'll likely
-          end up paired together in a 'coolrhour'
         </p>
       </div>
       <h3 class="enter">Click anywhere to enter!</h3>
