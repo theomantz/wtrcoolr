04022dee7ecb43a70d1198c2cbf04428991720c8
jhnegbrt <jhnegbrt@gmail.com>
2021-05-05 09:49:11 -0700

    renders modal component from modal switch

---
+++ frontend/src/components/splash/welcome.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './welcome.css'
+
+const Welcome = () =>{
+  return(
+    <div className="modal-background">
+      <div className="modal-child welcome-modal">
+        <div>Welcome to wtrcoolr</div>
+        <div>Click anywhere to enter!</div>
+      </div>
+    </div>
+  )
+}
+
+export default Welcome
