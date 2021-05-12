9f9303c5336d673bc829a1bfdca16fe387263ee1
heavyevy <everettsmith95@gmail.com>
2021-05-12 15:06:00 -0700

    Create interests slice of state

--- frontend/src/reducers/ui_reducer.js
+++ frontend/src/reducers/ui_reducer.js
@@ -3,12 +3,14 @@ import coolrReducer from './coolrs_reducer';
 import { combineReducers } from 'redux';
 import pausedReducer from './paused_reducer';
 import notifiedOfCoolrs from './notified_of_coolrs_reducer';
-import SocketReducer from './socket_reducer'
+import SocketReducer from './socket_reducer';
+import pairInterests from './pair_interests_reducer'
 
 export default combineReducers({
   modal: ModalReducer,
   currentCoolrs: coolrReducer,
   notifiedOfCoolrs: notifiedOfCoolrs,
   paused: pausedReducer,
-  socket: SocketReducer
+  socket: SocketReducer,
+  pairInterests
 })
