93dab826851d432baf275f28c50bedd87ef33ebb
jhnegbrt <jhnegbrt@gmail.com>
2021-05-16 14:58:43 -0700

    adds styling to pair info

--- frontend/src/components/coolr_video/match_info.js
+++ frontend/src/components/coolr_video/match_info.js
@@ -6,19 +6,24 @@ class MatchInfo extends React.Component{
   }
 
   renderInterests(){
-    debugger
+
     let interests = this.props.matchInfo.interests
-    let interestsArray = []
+    let interestsArray = [<h3>Interests:</h3>]
     for (let i =0; i < interests.length;i++){
       interestsArray.push(<li>{interests[i]}</li>)
     }
-    console.log(interestsArray)
     return interestsArray
   }
 
-  // renderNonStarters(){
+  renderNonStarters(){
+    let nonStarters = this.props.matchInfo.nonStarters
+    let nonStartersArray = [<h3>Non-Starters:</h3>]
+    for (let i =0; i < nonStarters.length;i++){
+      nonStartersArray.push(<li>{nonStarters[i]}</li>)
+    }
+    return nonStartersArray
 
-  // }
+  }
 
   render(){
 
@@ -26,12 +31,13 @@ class MatchInfo extends React.Component{
     console.log(matchInfo)
 
     return(
-      <div className="pair-infor-container">
+      <div className="pair-info-container">
         <h2>{matchInfo.username ? "Pair Info" : "Learn about your pair here!"}</h2>
         <div className="pair-info">
-          <h3>{matchInfo ? matchInfo.username : ""}</h3>
+          <h3>{matchInfo.username ? 'Username:' : ""}</h3>
+          <h4>{matchInfo ? matchInfo.username : ""}</h4>
           {matchInfo.interests ? this.renderInterests() : ""}
-          {/* {matchInfo.nonStarters ? this.renderNonStarters() : ""} */}
+          {matchInfo.nonStarters ? this.renderNonStarters() : ""}
         </div>
       </div>
 
